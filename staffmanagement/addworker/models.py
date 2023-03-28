cf0d6d7665df28824006ecee8bb73c32365685d2
Farrukh Nurmatov <unreal.mw@gmail.com>
2023-03-28 13:36:20 +0500

    Created basic folder, one app created, created one model for this app.

---
+++ staffmanagement/addworker/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Worker(models.Model):
+    name = models.CharField("Имя", max_length=30)
+    last_name = models.CharField("Фамилия", max_length=30)
+    patronymic = models.CharField("Отчество", max_length=30)
+    birth_date = models.DateTimeField("Дата рождения",)
