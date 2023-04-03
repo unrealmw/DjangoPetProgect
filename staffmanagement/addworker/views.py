2c87feab7bedda027651f9ce8b245c6b2ec7305f
Farrukh Nurmatov <unreal.mw@gmail.com>
2023-04-03 17:51:55 +0500

    created first view from model in add worker app

--- staffmanagement/addworker/views.py
+++ staffmanagement/addworker/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Worker
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Управление персоналом для всех!!!!")
+    s = "Список персонала: \r\n\r\n\r\n\r"
+    for worker in Worker.objects.order_by("birth_date"):
+        s += f"{worker.pk} : {worker.name} {worker.last_name} {worker.patronymic} {worker.birth_date} \r\n"
+    return HttpResponse(s, content_type="text/plain; charset=utf-8")
